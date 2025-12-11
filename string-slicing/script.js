97a3243505197d840c7f162e9d2ac1391b80569d
petergillgithub <petergillhmg@gmail.com>
2025-12-11 15:34:19 +0000

    string-slicing

---
+++ string-slicing/script.js
@@ -0,0 +1,23 @@
+// string slicing = creating a substring from a portion of another string.
+//                  string.slice(start, end)
+
+// const fullName = "Bro Code";
+
+// let firstName = fullName.slice(0, 3);
+// let lastName = fullName.slice(4, 8);
+// console.log(firstName);
+// console.log(lastName);
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// console.log(firstName);
+// console.log(lastName);
+
+const email = "petergill@gmail.com";
+
+// let username = email.slice(0, email.indexOf("@")); // petergill
+
+// let username = email.slice(email.indexOf("@")); //@gmail.com
+
+console.log(username);
