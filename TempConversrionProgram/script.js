ea6ef8f1daa77155c7c5932ede709bc9f1d36e64
petergillgithub <petergillhmg@gmail.com>
2025-12-12 19:46:26 +0000

    Temp Converstion Program

---
+++ TempConversrionProgram/script.js
@@ -0,0 +1,21 @@
+//Temprature Conversion Program
+
+const textBox = document.getElementById("textBox");
+const toFahernheit = document.getElementById("toFahernheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+  if (toFahernheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (toCelsius.checked) {
+    temp = Number(textBox.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent = temp.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Please Select a unit";
+  }
+}
