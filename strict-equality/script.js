23b76754fd392f31876bede9ccd8b95b94a223be
petergillgithub <petergillhmg@gmail.com>
2025-12-12 00:11:33 +0000

    commit

---
+++ strict-equality/script.js
@@ -0,0 +1,13 @@
+// = assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+//  != inequality operator
+// !== strict inequality operator
+
+let number = 2.2;
+
+if (number == 2.2) {
+  console.log("yes it is peter gill");
+} else {
+  console.log("it is not peter gill");
+}
