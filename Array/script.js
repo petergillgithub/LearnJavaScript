3631710fc1c74774e27d0929510c875c7500c3a5
petergillgithub <petergillhmg@gmail.com>
2025-12-13 16:18:16 +0000

    array

---
+++ Array/script.js
@@ -0,0 +1,33 @@
+// array = a variable like structure that can hold more than 1 value.
+
+// let fruits = ["apple", "orange", "mango", "kiwi"];
+
+// fruits[3] = "coconut"; //apple,orange,mango,coconut
+// fruits.push("coconut"); //apple,orange,mango,coconut
+// fruits.pop(); // apple,orange
+// fruits.unshift("lichi"); //lichi,apple,orange,mango,coconut
+// fruits.shift(); // orange,mango
+
+/* console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]); */
+
+/* let numoffruits = fruits.length;
+let index = fruits.indexOf("kiwi");
+
+console.log(index); */
+
+/* for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+} */
+
+//=================================================
+
+/* let fruits = ["apple", "orange", "mango", "kiwi"];
+
+fruits.sort().reverse();
+
+for (let fruit of fruits) {
+  console.log(fruit);
+} */
