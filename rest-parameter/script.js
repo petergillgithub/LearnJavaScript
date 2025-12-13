b293289e6f6186d3d081166e12fa9e27eb15a1f3
petergillgithub <petergillhmg@gmail.com>
2025-12-13 20:29:10 +0000

    rest parameter

---
+++ rest-parameter/script.js
@@ -0,0 +1,30 @@
+// rest parameter = (...rest) allows a function work with a variable number of arguments by bundling them into an array
+
+//                  spread = expands an array into seperate elements.
+//                  rest = bundles sperate elements into an array.
+/* 
+function collectFood(...foods) {
+  return foods;
+}
+
+const food1 = "roti";
+const food2 = "parantha";
+const food3 = "daal";
+const food4 = "paneer";
+
+const foods = collectFood(food1, food2, food3, food4);
+console.log(foods); */
+
+// ================================================
+
+/* function fruits(...allfruits) {
+  console.log(...allfruits); //apple mango lichi orange
+  // console.log(allfruits); //(4) ['apple', 'mango', 'lichi', 'orange']
+}
+
+const fruit1 = "apple";
+const fruit2 = "mango";
+const fruit3 = "lichi";
+const fruit4 = "orange";
+
+fruits(fruit1, fruit2, fruit3, fruit4); */
