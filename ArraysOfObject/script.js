10f467a3304c23f53305cac0b876b5b806bd0325
petergillgithub <petergillhmg@gmail.com>
2026-01-08 21:18:38 +0000

    Commited

---
+++ ArraysOfObject/script.js
@@ -0,0 +1,39 @@
+const fruits = [
+  { name: "apple", color: "red", calories: 130 },
+  { name: "orange", color: "orange", calories: 232 },
+  { name: "banana", color: "yellow", calories: 440 },
+  { name: "pineapple", color: "yellow", calories: 300 },
+];
+
+// console.log(fruits[2].name);
+
+// fruits.push({ name: "grapes", color: "green", calories: 323 });
+
+// console.log(fruits[4].name);
+
+// fruits.pop();
+// console.log(fruits);
+
+// ====================== forEach() ===========================
+
+/* fruits.forEach((fruit) => console.log(fruit.name));
+ */
+
+// =========================== map()===========================
+
+/* const fruitName = fruits.map((fruit) => fruit.name);
+const fruitColor = fruits.map((fruit) => fruit.color);
+const fruitCalories = fruits.map((fruit) => fruit.calories);
+
+console.log(fruitName); */
+
+// ============================= filter() =======================
+
+// const yelllowFruits = fruits.filter((fruit) => fruit.color === "yellow");
+// console.log(yelllowFruits);
+
+// const lowCalFruits = fruits.filter((fruitCal) => fruitCal.calories < 400);
+
+// console.log(lowCalFruits);
+
+// ============================= reduce() =======================
